cd6a2cea17b757bdf759098de2ed40ea50d73271
wangyiqiaoa <wangyiqiaoa@enn.cn>
2024-05-17 18:04:10 +0800

    240517

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -3,6 +3,7 @@ package com.itheima.controller;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
+import com.itheima.utils.JwtUtil;
 import com.itheima.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -47,7 +51,12 @@ public class UserController {
         //判断密码是否正确 loginUser对象中的password是密文
         if (Md5Util.getMD5String(password).equals(loginUser.getPassword())){
             //登录成功
-            return Result.success("jwt token..");
+
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", loginUser.getId());
+            claims.put("username", loginUser.getUsername());
+            String token = JwtUtil.genToken(claims);
+            return Result.success(token);
         }
 
         return Result.error("密码错误");
